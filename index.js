ab0341340c53ac309d1dd8d0fb6a141f6e15494c
muhamad luthfi sadli <luthfisad1806@gmail.com>
2024-10-14 16:47:05 +0700

    fix: destination

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 #!/usr/bin/env node
 
-import { cloneRepo } from '../lib/cloneRepo.js';
-import { logSuccess, logError } from '../lib/logger.js';
+import { cloneRepo } from './lib/cloneRepo.js';
+import { logSuccess, logError } from './lib/logger.js';
 
 const repoUrl = "https://github.com/LuthfiSad/express-template-luthfi.git"; // URL repositori Anda
-const projectName = process.argv[2] === "." ? process.cwd() : (process.argv[2] || "tsexpress-luthfi");// Nama folder proyek
+let projectName = process.argv[2] === "." ? process.cwd() : process.argv[2] || "expressts-luthfi"; // Nama folder proyek
 
 console.log('======================================');
-console.log('    Welcome to tsexpress-luthfi! ');
+console.log('    Welcome to expressts-luthfi! ');
 console.log('======================================');
 console.log('This will create a new Express project for you.');
 console.log('Please wait while we set everything up...');
