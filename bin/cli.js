143a7b49da7a097ab10dbdb0adfe4ce85ad6ec1b
muhamad luthfi sadli <luthfisad1806@gmail.com>
2024-10-14 14:11:09 +0700

    fix: folder name

--- bin/cli.js
+++ bin/cli.js
@@ -4,19 +4,19 @@ import { cloneRepo } from '../lib/cloneRepo.js';
 import { logSuccess, logError } from '../lib/logger.js';
 
 const repoUrl = "https://github.com/LuthfiSad/express-template-luthfi.git"; // URL repositori Anda
-const projectName = "expressts-luthfi"; // Nama folder proyek
+const projectName = process.argv[2] === "." ? process.cwd() : (process.argv[2] || "tsexpress-luthfi");// Nama folder proyek
 
 console.log('======================================');
-console.log(' Welcome to expressts-luthfi! ');
+console.log('    Welcome to tsexpress-luthfi! ');
 console.log('======================================');
 console.log('This will create a new Express project for you.');
 console.log('Please wait while we set everything up...');
 
 cloneRepo(repoUrl, projectName)
   .then(() => {
-    logSuccess('\n======================================');
+    logSuccess('\n=======================================');
     logSuccess('       Setup completed successfully!   ');
     logSuccess('          Happy Coding : ]            ');
-    logSuccess('======================================');
+    logSuccess('=======================================');
   })
   .catch((err) => logError("Error cloning repository:", err));
